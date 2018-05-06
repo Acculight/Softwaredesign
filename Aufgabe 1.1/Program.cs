fb609f9d811e324462f4d578e4f8c4f788f74634
Acculight <nkpka@gmx.de>
2018-05-06 18:25:30 +0200

    A1,2,4.1

---
+++ Aufgabe 1.1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Aufgabe1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double size = Convert.ToDouble(args[1]);
+
+            switch (args[0])
+            {
+                case "w":
+                    Console.WriteLine(getCubeInfo(size));
+                    break;
+                case "k":
+                    Console.WriteLine(getSphereInfo(size));
+                    break;
+                case "o":
+                    Console.WriteLine(getOctahedronInfo(size));
+                    break;
+                default:
+                    Console.WriteLine("Verstehe ich nicht");
+                    break;
+            }
+
+        }
+        //Methoden Würfel
+        public static double getCubeSurface(double size)
+        {
+            return Math.Round(6 * (size * size),2);
+        }
+        public static double getCubeVolume(double size)
+        {
+            return Math.Round((size * size * size),2);
+        }
+        public static string getCubeInfo(double size)
+        {
+            return "A=" + getCubeSurface(size) + " | " + "V=" + getCubeVolume(size);
+        }
+
+        //Methoden Kugel
+        public static double getSphereSurface(double size)
+        {
+            return Math.Round(Math.PI * (size * size),2);
+        }
+        public static double getSphereVolume(double size)
+        {
+            return Math.Round((Math.PI * (size * size * size)) / 6,2);;
+        }
+        public static string getSphereInfo(double size)
+        {
+            return "A=" + getSphereSurface(size) + " | " + "V=" + getSphereVolume(size);
+        }
+
+        //Method Oktaeder
+        public static double getOctahedronSurface(double size)
+        {
+            return Math.Round(2 * Math.Sqrt(3) * (size * size),2);
+        }
+        public static double getOctahedronVolume(double size)
+        {
+            return Math.Round((Math.Sqrt(2) * (size * size * size)) / 3,2);
+        }
+        public static string getOctahedronInfo(double size)
+        {
+            return "A=" + getOctahedronSurface(size) + " | " + "V=" + getOctahedronVolume(size);
+        }
+    }
+}
---
+++ Aufgabe 1.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Aufgabe1_2
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        static string Subject;
+        static string Verb;
+        static string Object;
+        static void Main(string[] args)
+        {
+            string[] verse = new string[subjects.Length];
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                GetVerse();
+                verse[i] = Subject + " " + Verb + " " + Object;
+            }
+            for (int j = 0; j < subjects.Length; j++)
+            {
+                Console.WriteLine(verse[j]);
+            }
+        }
+        public static void GetVerse()
+        {
+            Random random = new Random();
+            int randomSubject = random.Next(0, subjects.Length);
+            int randomVerb = random.Next(0, verbs.Length);
+            int randomObject = random.Next(0, objects.Length);
+            
+            while (subjects[randomSubject] == "used")
+            {
+                randomSubject = random.Next(0, subjects.Length);
+            }
+            while (verbs[randomVerb] == "used")
+            {
+                randomVerb = random.Next(0, verbs.Length);
+            }
+            while (objects[randomObject] == "used")
+            {
+                randomObject = random.Next(0, objects.Length);
+            }
+            Subject = subjects[randomSubject];
+            Verb = verbs[randomVerb];
+            Object = objects[randomObject];
+
+            subjects[randomSubject] = "used";
+            verbs[randomVerb] = "used";
+            objects[randomObject] = "used";
+        }
+    }
+}
