4692b930c01d08108d958fcc7a55add765a054cc
Acculight <nkpka@gmx.de>
2018-05-27 15:19:51 +0200

    Aufgabe 5
    Aufgabe 5

---
+++ Aufgabe 5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uml_aufgabe
+{
+    public class CoursesList<C>
+    {
+
+        class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public int Age;
+        }
+
+        class Teilnehmer : Person
+        {
+            public int MatriculationNumber;
+            public List<CoursesList<C>> course = new List<CoursesList<C>>();
+        }
+
+        class Dozent : Person 
+        {
+            public string Room;
+            public DateTime OfficeDays;
+            public DateTime OfficeHours;
+            public List<CoursesList<C>> course = new List<CoursesList<C>>();
+
+            public void OutputCourse(){
+                foreach (C course in Kurs)
+                {
+                    Console.WriteLine(Kurs.Title);
+                }
+            }c
+            public Participiant<C> Outputparticipants()
+            {
+                List<Participiant> participants = new List<Participiant>();
+                foreach (C course in Kurs)
+                {
+                    foreach(Participiant participiant in Teilnehmer.Participiant)
+                    {
+                        if(!participants.Contains(participiant))
+                        {
+                            participants.Add(participiant);
+                        }
+                    }
+                }
+                return participants;
+            }
+
+
+        }
+
+        class Kurs
+        {
+
+            public string Titel;
+            public DateTime Day;
+            public DateTime Time;
+            public string CourseRoom;
+
+            public Dozent Dozent;
+            List<Participiant> participantsT;
+            public void OutputInformation(){
+                Console.WriteLine("Kurs: " + Titel + " Tag: " + Day + " Uhrzeit:  " + Time + " Raum: " + CourseRoom);
+            }
+        }
+    }
+}
---
+++ Generics_Einführung/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+    public class Auto<PLATZHALTER>
+    {
+        public double PS;
+
+        public int Zylinder;
+
+        public double Hubraum;
+
+        public PLATZHALTER Farbe;
+    }
+
+
+
+
+namespace Generics_Einführung
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] meinIntegerArray = new int[10];
+            meinIntegerArray[0] = 1;
+            meinIntegerArray[1] = 1;
+            meinIntegerArray[2] = 1;
+            meinIntegerArray[3] = 1;
+            meinIntegerArray[4] = 1;
+            meinIntegerArray[5] = 1;
+            meinIntegerArray[6] = 1;
+            meinIntegerArray[7] = 1;
+            meinIntegerArray[8] = 1;
+            meinIntegerArray[9] = 1;
+
+
+
+
+            //Bestellwesen
+            Auto<string> meinAuto = new Auto<string>();
+
+            meinAuto.Farbe = "grau";
+            meinAuto.PS = 100;
+            meinAuto.Zylinder = 4;
+            meinAuto.Hubraum = 2.8;
+
+
+            //Produktion
+            Auto<int> einAnderesAuto = new Auto<int>();
+            einAnderesAuto.Farbe = 1;
+            einAnderesAuto.PS = 100;
+            einAnderesAuto.Zylinder = 4;
+            einAnderesAuto.Hubraum = 2.8;
+
+
+
+        }
+    }
+}
